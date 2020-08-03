d90e4ee19a5870c7fbb50c3eac373d6f301a027c
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-03 21:17:21 +0700

    Index Typescript

---
+++ client/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './style.scss';
+
+import App from './components/AppComponent/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
