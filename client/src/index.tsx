13681937248620a9e0aad6ec86fd888208534519
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-04 21:11:00 +0700

    Change Path Import App Component And Import CSS normal and reset

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import './style.scss';
+import './scss/generic/_normal.scss';
+import './scss/generic/_reset.scss';
 
-import App from './components/AppComponent/App';
+import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
