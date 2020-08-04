67f7ace378f1f776fc2a0c61b8ad32a6eb2029ec
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-04 21:09:27 +0700

    Config Minify JS and CSS Webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,40 +1,52 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src/index.tsx'),
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
     },
     module: {
         rules: [{
                 test: /\.ts(x?)$/,
                 exclude: /node_modules/,
-                use: ['ts-loader', 'eslint-loader']
+                use: ['ts-loader', 'eslint-loader'],
             },
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',
+                    MiniCSSExtractPlugin.loader,
                     {
                         loader: 'css-loader',
                         options: {
-                            modules: true
-                        }
+                            modules: {
+                                localIdentName: '[hash:base64:4]',
+                            },
+                        },
                     },
-                    'sass-loader'
-                ]
-            }
-        ]
+                    'sass-loader',
+                ],
+            },
+        ],
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html')
-        })
-    ]
-}
+            template: path.resolve(__dirname, 'src/index.html'),
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'css/style.css',
+        }),
+    ],
+};
