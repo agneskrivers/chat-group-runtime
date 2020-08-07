bc05ea4db5ed8ea91f70f1a8146bee9d82600fd1
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-07 11:36:48 +0700

    Style SCSS Component Signup

---
+++ client/src/components/Signup/_index.scss.d.ts
@@ -0,0 +1,7 @@
+export const signup: string;
+export const text: string;
+export const title: string;
+export const input: string;
+export const summit: string;
+export const login: string;
+export const error: string;
