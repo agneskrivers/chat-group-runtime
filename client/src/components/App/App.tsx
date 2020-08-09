4b04258a02df923ad83ff6588cafa8b3869450e9
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-09 22:41:51 +0700

    Component App: import component home

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,19 +1,22 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Component
 import Login from '../Login/index';
 import Signup from '../Signup/index';
+import Home from '../Home/index';
 
 const App: FunctionComponent = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    const [user, setUsers] = useState(null);
+
     return (
         <Router>
             <Switch>
                 <Route path='/signup'>
                     <Signup />
                 </Route>
-                <Route path='/'>
-                    <Login />
-                </Route>
+                <Route path='/'>{isLogin ? <Home /> : <Login />}</Route>
             </Switch>
         </Router>
     );
