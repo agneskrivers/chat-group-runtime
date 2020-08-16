1cac83d8e98b37aaeb9269fb614813d52c3886c5
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-16 08:00:19 +0700

    Add Link Profile Avatar User

--- client/src/components/User/index.tsx
+++ client/src/components/User/index.tsx
@@ -1,16 +1,46 @@
 import React, { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 
 // Style
 import Style from './_index.scss';
 
-const Profile: FunctionComponent = () => {
+interface Props {
+    avatar: string;
+    fullName: string;
+    logout: () => void;
+}
+
+const User: FunctionComponent<Props> = (props: Props) => {
+    const { avatar, fullName, logout } = props;
+
+    const urlAvatar: string =
+        avatar && avatar.length !== 0
+            ? avatar
+            : 'https://picsum.photos/200/300';
+
+    // Handle Event
+    const handleClickLougout = (): void => logout();
+
     return (
         <div className={Style.profile}>
-            <div className={Style.avatar}></div>
-            <div className={Style.name}>Test chat</div>
-            <div className={Style.logout}>Log Out</div>
+            <Link
+                to='/profile'
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textDecoration: 'none',
+                }}>
+                <div
+                    className={Style.avatar}
+                    style={{ backgroundImage: `url(${urlAvatar})` }}></div>
+                <div className={Style.name}>{fullName}</div>
+            </Link>
+            <div className={Style.logout} onClick={handleClickLougout}>
+                Log Out
+            </div>
         </div>
     );
 };
 
-export default Profile;
+export default User;
