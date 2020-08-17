f26f758547b9ed43f13cc2c1989551d0326f3a2a
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-17 18:33:17 +0700

    Update Router

---
+++ server/API/routers/profile.router.ts
@@ -0,0 +1,12 @@
+import express, { Router } from 'express';
+import multer, { Multer } from 'multer';
+
+import { postAvatar } from '../controllers/profile.controller';
+
+const router = Router();
+
+const upload: Multer = multer({ dest: '../../upload' });
+
+router.post('/', upload.single('avatar'), postAvatar);
+
+export default router;
