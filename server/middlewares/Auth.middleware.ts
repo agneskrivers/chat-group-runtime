bf01fac364873c4db86878c116a958ea3a523e7b
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-17 18:40:04 +0700

    Update Authencation Middleware

--- server/middlewares/Auth.middleware.ts
+++ server/middlewares/Auth.middleware.ts
@@ -2,22 +2,33 @@ import { Request, Response, NextFunction } from 'express';
 
 import { verifyToken } from '../helpers/jwt.helper';
 
-interface isAuthInterface {
-    (req: Request, res: Response, next: NextFunction): void;
+interface CustomRequest extends Request {
+    token?: string;
 }
 
-const accessTokenSecret: string | undefined = process.env.ACCESS_TOKEN_SECRET;
+const isAuth = async (
+    req: CustomRequest,
+    res: Response,
+    next: NextFunction,
+): Promise<void> => {
+    const token: string | string[] | undefined = req.headers['access-token'];
 
-const isAuth: isAuthInterface = async (req, res, next) => {
-    const token: string | undefined | string[] = req.headers['access-token'];
-    if (token && typeof token === 'string' && accessTokenSecret) {
-        try {
-            const decoded = await verifyToken(token, accessTokenSecret);
-            console.log(decoded);
-            res.locals.token = decoded;
-        } catch (error) {
-            localStorage.removeItem('token');
+    try {
+        if (
+            token &&
+            typeof token === 'string' &&
+            process.env.ACCESS_TOKEN_SECRET
+        ) {
+            const decoded = await verifyToken(
+                token,
+                process.env.ACCESS_TOKEN_SECRET,
+            );
+
+            next();
         }
+    } catch (error) {
+        localStorage.removeItem('token');
+        next();
     }
 
     next();
