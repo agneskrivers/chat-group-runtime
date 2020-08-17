890449a49208aa90f9151c4556f60de30e6c15b0
Agnes K. Rivers <agneskrivers@gmail.com>
2020-08-17 18:38:55 +0700

    Import Router APIs And Using

--- server/API/api.ts
+++ server/API/api.ts
@@ -3,11 +3,13 @@ import express, { Router } from 'express';
 import LoginAPI from './routers/login.router';
 import SignupAPI from './routers/signup.router';
 import CheckAPI from './routers/check.router';
+import ProfileAPI from './routers/profile.router';
 
 const router = Router();
 
 router.use('/login', LoginAPI);
 router.use('/signup', SignupAPI);
 router.use('/check', CheckAPI);
+router.use('/profile', ProfileAPI);
 
 export default router;
